c8a8ff98fdbd825d02434cc93eee03488eea9405
Michael Donnelly <themickster268@gmail.com>
2022-10-16 19:53:38 +0100

    Authentication Functionality Added

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using dotnet_rpg.Services.CharacterService;
 using dotnet_rpg.Dtos.Character;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace dotnet_rpg.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
@@ -21,7 +24,8 @@ namespace dotnet_rpg.Controllers
 
         [HttpGet("GetAll")]
         public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> GetAllCharacters(){
-            return Ok(await this._characterService.GetAllCharacters());
+            int userId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            return Ok(await this._characterService.GetAllCharacters(userId));
         }        
         
         [HttpGet("{id}")]
--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -13,6 +13,8 @@ namespace dotnet_rpg.Data
             
         }
 
-        public DbSet<Character> Characters { get; set; }
+        public DbSet<Character> Characters => Set<Character>();
+
+        public DbSet<User> Users => Set<User>();
     }
 }
